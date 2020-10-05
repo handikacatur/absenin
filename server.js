7d30cc938f8fa4f321c8e0fe650faa0ee1207662
handikacatur <192410101066@students.unej.ac.id>
2020-10-05 15:36:06 +0700

    add get activities function & login function

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const { text } = require('express');
 const puppeteer = require('puppeteer');
 
 const loginUrl = 'https://mmp.unej.ac.id/login/index.php';
 
-const users = [];
-
 const tasks = [];
+
+let loginAttempt = 5;
 
 const login = async (page) => {
     /* Login function for every core */
@@ -16,10 +17,29 @@ const login = async (page) => {
     await page.keyboard.type('192410101066');
     await page.keyboard.press('Tab');
     await page.keyboard.type('Dhyka_67');
+    await page.keyboard.press('Enter');
     
-    const loginButton = await page.$('input[type="submit"]');
-    await loginButton.click();
+    await page.waitForNavigation({waitUntil: 'networkidle0'});
+
+    const title = await page.evaluate(() => Array.from(
+        document.querySelectorAll('title'),
+        title => title.innerText
+    ));
+
+    // Check if user has signed in
+    if (title[0] === 'Dashboard') {
+        return 'Login Success!';
+    } else {
+        if (loginAttempt === 0) {
+            return 'Login Failed! Check your password and try again.';
+        } else {
+            loginAttempt -= 1;
+            page.reload();
+            login(page);
+        }
+    }
 }
+
 
 const getActivites = async () => {
     /* Get current activities */
@@ -28,20 +48,34 @@ const getActivites = async () => {
 
     await login(page);
 
-    await page.waitForNavigation({waitUntil: 'networkidle0'});
-
-    // const today = await page.$('td.today');
-    const today = await page.$$('td.day'); // this is for test 
-    await today[0].click();
+    const today = await page.$('td.today');
+    await today.click();
 
     await page.waitForNavigation({waitUntil: 'networkidle0'});
 
-    const texts = await page.evaluate(() => Array.from(
+    let texts = await page.evaluate(() => Array.from(
         document.querySelectorAll('span.date'),
         span => span.innerText
     ));
 
-    console.log(texts);
+    texts = texts.map((i) => i.split(' ')[1].split(':'));
+
+    texts.forEach((i) => {
+        const currentActivities = new Date();
+        currentActivities.setHours(parseInt(i[0]));
+        currentActivities.setMinutes(parseInt(i[1]));
+
+        tasks.push([currentActivities]);
+    });
+
+    const links = await page.evaluate(() => Array.from(
+        document.querySelectorAll('div.calendar_event_attendance a'),
+        a => a.getAttribute('href')
+    ));
+
+    for (key in links) {
+        tasks[key].push(links[key]);
+    }
 } 
 
 const core1 = async () => {
@@ -55,5 +89,9 @@ const core1 = async () => {
 //   await browser.close();
 }
 
-getActivites();
-// core1();
+const main = async () => {
+    getActivites();
+    // core1();
+}
+
+main();
